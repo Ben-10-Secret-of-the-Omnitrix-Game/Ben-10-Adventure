d7185f55d61148bf788e60a2641843ca3284462d
Dmitry Molokovich <molokovich.dima@gmail.com>
2021-01-17 15:43:03 +0300

    hot reloading enabled

--- ben_ten_adventure/__main__.py
+++ ben_ten_adventure/__main__.py
@@ -6,14 +6,28 @@ finally should be like:
 if __name__ == '__main__':
     engine.start()
 """
-from . import game_engine
 import logging
 import coloredlogs
+import pygame
+
+from hotreload import Loader
+from os.path import join
 
 from .tests import utils_test
+from . import game_engine
+
 
 if __name__ == '__main__':
     coloredlogs.install(level='DEBUG')
-    game_engine.start()
-    utils_test.run_tests()
+    script = Loader(join("ben_ten_adventure", "game_engine.py"), "ben_ten_adventure.game_engine", 1)
+    
+    game_engine.init()
+    script.start()
+    
+    while True:
+        if script.has_changed():
+            pygame.time.wait(500)
+        script.game_loop_handler()
+
+    # utils_test.run_tests()
     
--- ben_ten_adventure/game_engine.py
+++ ben_ten_adventure/game_engine.py
@@ -11,24 +11,44 @@ from .utils import GameAssets, init_resource_dirs
 def draw_main_ui():
     pass
 
-def start():
+def init():
+    global screen
     pygame.init()
     pygame.font.init()
     
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((500, 500))
     
+def start():
+    global ga, clock, fps
     # .utils.py
     init_resource_dirs()
     ga = GameAssets()
+    clock = pygame.time.Clock()
+    fps = 30
     
-    screen.blit(ga.play_button, (0, 0, 32, 32))
-    screen.blit(ga.play_button_64, (0, 32, 64, 64))
-    pygame.display.flip()
-    while True:
-        event = pygame.event.poll()
+    # screen.blit(ga.background, (0, 0, 500, 500))
+    # screen.blit(ga.play_button, (0, 0, 32, 32))
+    # screen.blit(ga.play_button_64, (250, 160, 64, 64))
+    # screen.blit(ga.ben10_1, (150, 150, 70, 205))
+    # pygame.display.flip()
+    # while True:
+    #     event = pygame.event.poll()
+    #     if event.type == pygame.QUIT:
+    #         pygame.quit()
+    #         sys.exit()
+        
+    #     pygame.display.flip()
+
+def game_loop_handler():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.image.save(screen, 'hi_man.png')
             pygame.quit()
             sys.exit()
         
-        pygame.display.flip()
-
+    clock.tick(fps)
+    screen.blit(ga.background, (0, 0, 500, 500))
+    screen.blit(ga.play_button, (0, 0, 32, 32))
+    screen.blit(ga.play_button_64, (200, 160, 64, 64))
+    screen.blit(ga.ben10_1, (150, 150, 70, 205))
+    pygame.display.flip()
