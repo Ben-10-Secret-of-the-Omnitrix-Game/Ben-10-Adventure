79822fd42fdc21343e1b2381d780bff8eb4cb8d3
Dmitry Molokovich <molokovich.dima@gmail.com>
2021-01-17 14:55:11 +0300

    added GameAssets test

--- ben_ten_adventure/tests/utils_test.py
+++ ben_ten_adventure/tests/utils_test.py
@@ -1,16 +1,15 @@
-from ..utils import init_resource_dirs
+from ..utils import GameAssets, init_resource_dirs
 import logging
-import coloredlogs
 
 
 
 def test_init_resource_dirs():
     init_resource_dirs()
 
+def test_GameAssets():
+    ga = GameAssets()
 
 def run_tests():
-    logger = logging.getLogger(__name__)
-    coloredlogs.install(level='DEBUG', logger=logger)
-    
     test_init_resource_dirs()
-    logger.info("Tests completed")    
+    test_GameAssets()
+    logging.info("Tests completed")
