9b524da344c4975802f5c101725fbda7d8afcd76
Dmitry Molokovich <molokovich.dima@gmail.com>
2021-01-17 14:54:04 +0300

    displaying game assets(test only purpose)

--- ben_ten_adventure/game_engine.py
+++ ben_ten_adventure/game_engine.py
@@ -4,7 +4,9 @@ Contains pygame.init() and related stuff
 """
 
 import pygame
-from .utils import init_resource_dirs
+import sys
+
+from .utils import GameAssets, init_resource_dirs
 
 def draw_main_ui():
     pass
@@ -12,4 +14,21 @@ def draw_main_ui():
 def start():
     pygame.init()
     pygame.font.init()
+    
+    screen = pygame.display.set_mode((1280, 720))
+    
+    # .utils.py
+    init_resource_dirs()
+    ga = GameAssets()
+    
+    screen.blit(ga.play_button, (0, 0, 32, 32))
+    screen.blit(ga.play_button_64, (0, 32, 64, 64))
+    pygame.display.flip()
+    while True:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+        pygame.display.flip()
 
