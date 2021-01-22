eb8363524984c2d68e6ed38f3b6e55a0142d19c7
Ivan <izlatin@yandex.ru>
2021-01-22 11:37:42 +0300

    again screen quality

--- ben_ten_adventure/game_engine.py
+++ ben_ten_adventure/game_engine.py
@@ -10,23 +10,41 @@ import sys
 from .utils import GameAssets, init_resource_dirs
 from .graphics import Tile
 from hotreload import Loader
+
 from os.path import join
+from os import environ
+
+import ctypes
+
+environ['SDL_VIDEO_CENTERED'] = '1'
+user32 = ctypes.windll.user32
+screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
+if screensize[0] > 1920:
+    win_size = (1920, 1080)
+elif screensize[0] > 1280:
+    win_size = (1280, 720)
+else:
+    win_size = (720, 480)
+
 
 def draw_main_ui():
     pass
+
 
 def init():
     global screen, MAP_WIDTH, MAP_HEIGHT, TILE_SIZE, border_offset
     pygame.init()
     pygame.font.init()
-    
-    screen = pygame.display.set_mode((1280, 720), flags=pygame.RESIZABLE)
+
+    screen = pygame.display.set_mode((win_size), flags=pygame.RESIZABLE)
     MAP_WIDTH = 5
     MAP_HEIGHT = 5
     TILE_SIZE = (32, 32)
     # TODO replace with more approximate offset
     border_offset = screen.get_width() / 2 + TILE_SIZE[0] * MAP_WIDTH
-    
+
+
 def start():
     global ga, clock, fps, script
     script = Loader(join("ben_ten_adventure", "graphics.py"), "ben_ten_adventure.graphics", 1)
@@ -35,7 +53,7 @@ def start():
     ga = GameAssets()
     clock = pygame.time.Clock()
     fps = 30
-    
+
 
 def game_loop_handler():
     global border_offset
@@ -64,5 +82,3 @@ def game_loop_handler():
     pygame.display.flip()
     # Carefull! border_offset need to convert to isometrix coordinates first! 
     # pygame.display.update((border_offset, border_offset / 2, border_offset, border_offset / 2))
-    
-
