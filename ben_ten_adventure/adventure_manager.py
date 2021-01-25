593adf1307d3f9c32db40ad46223f2228b18cb01
Dmitry Molokovich <molokovich.dima@gmail.com>
2021-01-25 13:12:50 +0300

    Secret Of The Omnitrix; adventure structure

--- ben_ten_adventure/adventure_manager.py
+++ ben_ten_adventure/adventure_manager.py
@@ -5,6 +5,7 @@ class Adventure:
     * Any adventure must inherit be inherited from it.
     """
     pass
+        
 
 class AdventureManager:
     """
@@ -17,3 +18,44 @@ class AdventureManager:
         that's all by now
     """
     pass
+
+class SecretOfTheOmnitrix(Adventure):
+    def __init__(self):
+        self.stages = [
+            self.show_intro,
+            self.start_scene_1,
+            self.show_intro_2,
+            self.start_scene_2,
+            self.start_scene_3]
+        
+    def show_intro(self):
+        """
+        First video. 
+        """
+        pass
+    
+    def start_scene_1(self):
+        """
+        Fight between Ben 10 and prisoners. Save Myaxx
+        """
+        pass
+    
+    def show_intro_2(self):
+        """
+        Flying to Azmuth's planet
+        """
+        pass
+    
+    def start_scene_2(self):
+        """
+        Fighting with Vilgax
+        """
+        pass
+    
+    def start_scene_3(self):
+        """
+        Urge Azmuth to keep omnitrix in Ben's hand.  
+        """
+        pass
+    
+    
